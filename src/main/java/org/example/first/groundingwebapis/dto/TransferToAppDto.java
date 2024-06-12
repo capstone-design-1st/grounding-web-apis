7bc92181db701fc15e9f2c4bb020b0ad69e4b39f
edwardohjs <edwardohjs99@gmail.com>
2024-06-12 21:44:43 +0900

    modify TransferToAppDto

--- src/main/java/org/example/first/groundingwebapis/dto/TransferToAppDto.java
+++ src/main/java/org/example/first/groundingwebapis/dto/TransferToAppDto.java
@@ -1,95 +1,90 @@
 package org.example.first.groundingwebapis.dto;
+
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import org.example.first.groundingwebapis.entity.AssetFiles;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class TransferToAppDto {
-
-    // From InvestmentPieceRequest
-    private String type;
-    private String location;
-    private Integer price;
-    private String info;
-    private Integer floors;
-    private Integer pieceCount;
-    private String useArea;
-    private String mainUse;
-    private double landArea;
-    private double totalArea;
-    private double buildingToRandRatio;
-    private Integer floorAreaRatio;
-    private String buildingDate;
-    private boolean automaticCloseFlag;
-    private String assetType;
-    private String entryStatus;
-    private String landClassification;
-    private String recommendedUse;
-    private String desiredPrice;
-    private Integer pricePerUnit;
-    private String investmentPoint;
-    private String landImageRegistration;
-    private String leaseStartDate;
-    private String leaseEndDate;
-    private String assetImage;
-    private String assetCertificateUrl;
 
     // From NewsDto
     private String newsTitle;
     private String reportedAt;
     private String publisher;
 
+    // From AdminAssetFileListsResponse
+    private String fileName;
+
     // From MainMyResponse
     private List<AssetFiles> thumbnailImage;
 
-    // Constructor with NewsDto Builder fields
+    private Estate estate;
+    private Land land;
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
     @Builder
-    public TransferToAppDto(String type, String location, Integer price, String info, Integer floors, Integer pieceCount, String useArea,
-                         String mainUse, double landArea, double totalArea, double buildingToRandRatio, Integer floorAreaRatio,
-                         String buildingDate, boolean automaticCloseFlag, String assetType, String entryStatus, String landClassification,
-                         String recommendedUse, String desiredPrice, Integer pricePerUnit, String investmentPoint, String landImageRegistration,
-                         String leaseStartDate, String leaseEndDate, String assetImage, String assetCertificateUrl,
-                         String newsTitle, String reportedAt, String publisher, List<AssetFiles> thumbnailImage) {
-        this.type = type;
-        this.location = location;
-        this.price = price;
-        this.info = info;
-        this.floors = floors;
-        this.pieceCount = pieceCount;
-        this.useArea = useArea;
-        this.mainUse = mainUse;
-        this.landArea = landArea;
-        this.totalArea = totalArea;
-        this.buildingToRandRatio = buildingToRandRatio;
-        this.floorAreaRatio = floorAreaRatio;
-        this.buildingDate = buildingDate;
-        this.automaticCloseFlag = automaticCloseFlag;
-        this.assetType = assetType;
-        this.entryStatus = entryStatus;
-        this.landClassification = landClassification;
-        this.recommendedUse = recommendedUse;
-        this.desiredPrice = desiredPrice;
-        this.pricePerUnit = pricePerUnit;
-        this.investmentPoint = investmentPoint;
-        this.landImageRegistration = landImageRegistration;
-        this.leaseStartDate = leaseStartDate;
-        this.leaseEndDate = leaseEndDate;
-        this.assetImage = assetImage;
-        this.assetCertificateUrl = assetCertificateUrl;
-        this.newsTitle = newsTitle != null ? newsTitle : "";
-        this.reportedAt = reportedAt != null ? reportedAt : LocalDate.now().format(DateTimeFormatter.ISO_DATE);
-        this.publisher = publisher != null ? publisher : "";
-        this.thumbnailImage = thumbnailImage;
+    @JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
+    public static class Estate {
+        private String assetType;
+        private String location;
+        private double total_area;
+        private String investmentPoint;
+        private String assetCertificateUrl;
+        private String entryStatus;
+        private String leaseStartDate;
+        private String leaseEndDate;
+        private Integer pricePerUnit;
+        private String assetName;
+        private String walletAddress;
+        private Integer piece_count;
+        private Integer floors;
+        private String use_area;
+        private String main_use;
+        private double land_area;
+        private double building_to_rand_ratio;
+        private double floor_area_ratio;
+        private String building_date;
+        private boolean automatic_close_flag;
+        private String assetImage;
+        private Integer price;
+        private String type;
+    }
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    @JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
+    public static class Land {
+        private String assetType;
+        private String location;
+        private double total_area;
+        private String investmentPoint;
+        private String assetCertificateUrl;
+        private String entryStatus;
+        private String leaseStartDate;
+        private String leaseEndDate;
+        private Integer pricePerUnit;
+        private String assetName;
+        private String walletAddress;
+        private String landClassification;
+        private String recommendedUse;
+        private String desiredPrice;
+        private String info;
+        private String landImageRegistration;
+        private Integer price;
+        private String type;
     }
 }
