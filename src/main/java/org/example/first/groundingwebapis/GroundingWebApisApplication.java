c7757d1bf28420f7affa58447f8cff83880a26b3
Subin <70842920+bin-pro@users.noreply.github.com>
2024-06-13 11:24:57 +0900

    Test: Deploy

--- src/main/java/org/example/first/groundingwebapis/GroundingWebApisApplication.java
+++ src/main/java/org/example/first/groundingwebapis/GroundingWebApisApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GroundingWebApisApplication {
-
+//
     public static void main(String[] args) {
         SpringApplication.run(GroundingWebApisApplication.class, args);
     }
